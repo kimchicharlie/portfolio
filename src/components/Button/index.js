e0e5e7a4d750d0ff4678dfc2608be0038cdb7037
Charlie Henin <charlie.henin@sigfox.com>
2020-05-12 22:38:15 +0200

    feat: enable animation button

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+const Button = props => <button className="super-button" {...props} />;
+
+Button.propTypes = {};
+
+Button.defaultProps = {};
+
+export default Button;
--- src/views/Resume/index.js
+++ src/views/Resume/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PaperAnimation from '@components/PaperAnimation';
+import Button from '@components/Button';
 import LeftColumn from './LeftColumn';
 import RightColumn from './RightColumn';
 import './style.css';
 
-const Resume = () => (
-  <div className="resume-container">
-    <PaperAnimation className="resume-animated">
-      <LeftColumn />
-      <RightColumn />
-    </PaperAnimation>
+const Resume = () => {
+  const [animated, setAnimated] = useState(false);
+  return (
     <div className="resume">
-      <LeftColumn />
-      <RightColumn />
+      <div className="resume-button-container">
+        <Button onClick={() => setAnimated(!animated)}>
+          {`${animated ? 'Disable' : 'Enable'} animation`}
+        </Button>
+      </div>
+      <div className="resume-container">
+        {animated ? (
+          <PaperAnimation className="resume-paper">
+            <LeftColumn />
+            <RightColumn />
+          </PaperAnimation>
+        ) : (
+          <div className="resume-paper">
+            <LeftColumn />
+            <RightColumn />
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Resume;
